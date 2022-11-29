5a6edc1df5a0dec362a4f5e5ad3ae56fb8acf9d5
letmarchi <leticiamarchi@id.uff.br>
2022-11-28 21:05:08 -0300

    final

--- main.c
+++ main.c
@@ -165,50 +165,83 @@ double Gauss(double **Matriz, int m, int n, double *x0, int p){
 }
 
 
-double Conjugado(double **Matriz, int m, int n, double *x0, double *d, int p){
+double Conjugado(double **M, int m, int n, double *x0, double *d, int p, double *l1){
   int i,j;
-  double a, *Ad, rr, *r, B, *r1;
+  double *r, a, b, *aux, *aux1, ort;
+   
+  r = (double *)malloc(m*sizeof(double));
+  aux = (double *)malloc(m*sizeof(double));
+ 
+  aux1 = (double *)malloc(m*sizeof(double));
 
-  r=r1=(double *) malloc(m *sizeof(double));
-  r=r1=Residuos(Matriz, m, n, x0); 
-  Ad=MultiVetMat(Matriz, d, m, n);
-  rr=MultVetor(r,r,m);
-  a=rr/MultVetor(d, Ad, m);
   
-  for(i=0;i<m;i++){
-    x0[i]+=a*d[i];
-    r[i]-=a*Ad[i];
-  }
-  B=MultVetor(r,r,m)/rr;
-  for(i=0;i<m;i++){
-    d[i]=r[i]+B*d[i];
-  }
+  r = Residuos(M, m, n, x0);
+
+	for(i=0;i<m;i++){
+		for(j=0;j<m;j++){
+			if(i==j){
+				ort = r[i]*l1[j] + ort;
+			} 
+
+		}
+	}
+
+  d = Residuos(M, m, n, x0);
+
+  aux1 = r;
+
+  a = (MultVetor(r,r,m))/(MultVetor(r,MultiVetMat(M, r, m, n),m));
+
+  for(i=0;i<m;i++) x0[i] = x0[i] + a*d[i];
   
-  printf("%lf", MultVetor(r, r1, m));
-  puts("");
-  return NormaVetor(r, m, p);
+  aux = MultiVetMat(M, d, m, n);
+
+  l1 =r;
+	if(ort == 0){
+		printf("Os residuos sao ortogonais\n");
+	}
+
+  for(i=0;i<m;i++) r[i] = r[i] - a*aux[i];
+
+  b = (MultVetor(r,r,m))/(MultVetor(aux1,aux1,m));
+
+  for(i=0;i<m;i++) d[i] = d[i] + b*d[i];
+
+  return(NormaVetor(r, m, p));
 }
 
-double Jacobi(double **Matriz, int m, int n, double *x0, int p){
+double MaximaDescida(double **M, int m, int n, double *x0, int p, double *l1){
+
+  double *lt, *t, lambda, ort=0;
+  
   int i,j;
-  double sum, *v, *r;
 
-  v = (double *) malloc(m *sizeof(double));
+  t = (double *)malloc(m*sizeof(double));
+  
+  t = Residuos(M, m, n,x0);
 
   for(i=0;i<m;i++){
-    sum=0;
-    for(j=0;j<n-1;j++){
-      if(i!=j) sum+=Matriz[i][j]*x0[j];
-    }
-    v[i]=(Matriz[i][n-1]-sum)/Matriz[i][i];
-  }
-  r = (double *) malloc(m *sizeof(double));
-  for(i=0;i<m;i++){
-    r[i]=x0[i]-v[i];
-  }
-  memcpy(x0, v, m*sizeof(double));
-  return NormaVetor(r, m, p);
+		for(j=0;j<m;j++){
+			if(i==j){
+				ort = t[i]*l1[j] + ort;
+			} 
+
+		}
+	}
+
+  lambda=  (MultVetor(t,t,m))/(MultVetor(t,MultiVetMat(M, t, m, n),m));
+
+  l1 =t;
+
+  for(i=0;i<m;i++) x0[i]+=lambda*t[i];
+
+	if(ort == 0){
+		printf("Os residuos sao ortogonais\n");
+	}
+
+  return(NormaVetor(t, m, p));
 }
+
 
 int main() {
 double **M, **C,*b ,*v,*d,*l1, dx, tolerance=1e-8;
@@ -218,6 +251,94 @@ FILE *arq;
 M = LeMatriz("Matrix.dat",&m, &n);
 v = LeVetor("vetor.dat", &l);
 
+l1 = (double*)calloc(m,sizeof(double));
+
+printf("\n Maxima descida\n");
+do{
+  it++;
+  dx = MaximaDescida(M,m,n,v,p,l1);
+  printf("It:%d %8.4g\n", it, dx);
+  for( i=0; i<m; i++) printf("%11.6g ", v[i]);
+  puts("");
+}while (dx > tolerance);
+
+free(l1);
+
+l1 = (double*)calloc(m,sizeof(double));
+
+v = LeVetor("vetor.dat", &l);
+d = v;
+it =0;
+printf("\ngradiente conjugado\n");
+do{
+  it++;
+  dx = Conjugado(M,m,n,v,d,p, l1);
+  printf("It:%d %8.4g\n", it, dx);
+  for( i=0; i<m; i++) printf("%11.6g ", v[i]);
+  puts("");
+}while (dx > tolerance);
+
+free(l1);
+
+v = LeVetor("vetor.dat", &l);
+it =0;
+printf("\nGauss\n");
+do{
+  it++;
+  dx = Gauss(M,m,n,v,p);
+  printf("It:%d %8.4g\n", it, dx);
+  for( i=0; i<m; i++) printf("%11.6g ", v[i]);
+  puts("");
+}while (dx > tolerance);
+
+free(l1);
+
+printf("Letra c");
+
+M = LeMatriz("Matrix1.dat",&m, &n);
+v = LeVetor("vetor.dat", &l);
+
+l1 = (double*)calloc(m,sizeof(double));
+
+printf("\n Maxima descida\n");
+do{
+  it++;
+  dx = MaximaDescida(M,m,n,v,p,l1);
+  printf("It:%d %8.4g\n", it, dx);
+  for( i=0; i<m; i++) printf("%11.6g ", v[i]);
+  puts("");
+}while (dx > tolerance);
+
+free(l1);
+
+l1 = (double*)calloc(m,sizeof(double));
+
+v = LeVetor("vetor.dat", &l);
+d = v;
+it =0;
+printf("\ngradiente conjugado\n");
+do{
+  it++;
+  dx = Conjugado(M,m,n,v,d,p, l1);
+  printf("It:%d %8.4g\n", it, dx);
+  for( i=0; i<m; i++) printf("%11.6g ", v[i]);
+  puts("");
+}while (dx > tolerance);
+
+free(l1);
+
+v = LeVetor("vetor.dat", &l);
+it =0;
+printf("\nGauss\n");
+do{
+  it++;
+  dx = Gauss(M,m,n,v,p);
+  printf("It:%d %8.4g\n", it, dx);
+  for( i=0; i<m; i++) printf("%11.6g ", v[i]);
+  puts("");
+}while (dx > tolerance);
+
+free(l1);
 
 return 0;
 }
